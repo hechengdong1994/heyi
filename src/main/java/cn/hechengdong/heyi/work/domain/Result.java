4351499a53579ea4ab83800b3f97e299653e22b4
ChengDong He <569186076@qq.com>
2021-08-12 14:39:22 +0800

    code format

--- src/main/java/cn/hechengdong/heyi/work/domain/Result.java
+++ src/main/java/cn/hechengdong/heyi/work/domain/Result.java
@@ -2,6 +2,9 @@ package cn.hechengdong.heyi.work.domain;
 
 import java.util.Map;
 
+/**
+ * result of step executing
+ */
 public class Result {
     private boolean success;
     private String message;
